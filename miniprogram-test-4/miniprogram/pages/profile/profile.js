7f2ec4178bf8bedbfbbfa618f5622f959168f9e4
mdc <jokerm_13@163.com>
2019-06-29 23:26:52 +0800

    个人界面以及调查问卷的功能导航（添加已完成，查看未完成）

---
+++ miniprogram-test-4/miniprogram/pages/profile/profile.js
@@ -0,0 +1,82 @@
+// pages/profile/profile.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  addQuery : function(event){
+    console.log("添加调查问卷");
+    console.log(event);
+    wx.navigateTo({
+      url: "../addQueryPage/addQueryPage",
+    });
+  },
+
+  checkQuery: function (event) {
+    console.log("查看自己的调查问卷");
+    console.log(event);
+    wx.navigateTo({
+      url: "../addQueryPage/addQueryPage",
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
